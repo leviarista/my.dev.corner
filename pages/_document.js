53e1b53fc8c0c568a539e981d2a4c8fd9d7a6c78
Leví Arista <levi.arista@gmail.com>
2022-05-23 11:47:42 -0500

    Adding analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
         <Html>
             <Head>
-                {/* <title>My Dev Corner</title>
-                <meta name="description" content="The dev's favorite corner" />
-                <link rel="icon" href="/favicon.ico" /> */}
-
                 {/* Fonts */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap" rel="preload" as="style" />
                 <link href="https://fonts.googleapis.com/css2?family=ABeeZee:ital@0;1&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet" />
+                <script
+                    id="cunter.dev-analytics-init"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"levi",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
+                    }}
+                />
             </Head>
             <body>
                 <Main />
