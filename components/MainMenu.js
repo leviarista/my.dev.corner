479977badfeec368a00de4893869a99fcdcf961f
Leví Arista <levi.arista@gmail.com>
2022-06-07 11:05:25 -0500

    Info y credits modal

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -3,6 +3,7 @@ import Button from './buttons/Button';
 
 const MainMenu = ({ handleClickAddButton, handleClickTuneButton }) => {
 	const [showMoreOptions, setShowMoreOptions] = useState(false);
+	const [showInfoModal, setShowInfoModal] = useState(false);
 
 	const resetSettings = () => {
 		localStorage.clear();
@@ -39,6 +40,79 @@ const MainMenu = ({ handleClickAddButton, handleClickTuneButton }) => {
 					<a href='#' className='btn-link' onClick={() => resetSettings()}>
 						Reset settings
 					</a>
+					<a href='#' className='btn-link' onClick={() => setShowInfoModal(true)}>
+						Info & Credits
+					</a>
+				</div>
+			)}
+			{showInfoModal && (
+				<div id='info-modal' className='modal'>
+					<div>
+						<a
+							href='#'
+							title='Close'
+							className='modal-close'
+							onClick={() => setShowInfoModal(false)}
+						>
+							<img
+								src='/img/icons/close-circle.svg'
+								width='15'
+								height='15'
+								alt='close icon'
+							/>
+						</a>
+						<div className='modal-title'>
+							<b>Info & Credits</b>
+						</div>
+						<p>
+							Quotes from:
+							<span
+								style={{
+									zIndex: 50,
+									fontSize: '0.9em',
+									fontWeight: 'bold',
+									display: 'flex',
+									marginTop: '10px',
+								}}
+							>
+								<img
+									src='https://theysaidso.com/branding/theysaidso.png'
+									height='20'
+									width='20'
+									alt='theysaidso.com'
+								/>
+								<a
+									href='https://theysaidso.com'
+									title='Powered by quotes from theysaidso.com'
+									style={{
+										color: '#ccc',
+										marginLeft: '4px',
+										verticalAlign: 'middle',
+									}}
+								>
+									They Said So®
+								</a>
+							</span>
+						</p>
+						<div className='modal-info-text'>
+							<br />
+							<a
+								href='https://github.com/leviarista/my.dev.corner'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								Github Repository
+							</a>
+							<br />
+							<a
+								href='https://github.com/leviarista/my.dev.corner/blob/main/LICENSE'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								MIT License
+							</a>
+						</div>
+					</div>
 				</div>
 			)}
 		</>
